c532d857892f16ad9474428643930e63207d5c6e
ebanza276-wq <ebanza276@gmail.com>
2026-09-05 16:57:32 +0200

    Add allow_unsafe_werkzeug option to socketio.run

--- main.py
+++ main.py
@@ -529,5 +529,6 @@ def get_recupere(user_id):
         })
     return jsonify(result)
 if __name__ == "__main__":
-    socketio.run(app, port=5000,debug=True)
+    socketio.run(app, port=5000, debug=True, allow_unsafe_werkzeug=True)
+    #socketio.run(app, port=5000,debug=True)
     #app.run(debug=True)
